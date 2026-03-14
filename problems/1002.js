1fc16d490c0be39e1446485e7b5e51cdb34636d7
Jhon Gomes <jhon.gomes1368@gmail.com>
2026-03-13 23:49:34 -0300

    Add files via upload

---
+++ problems/1002.js
@@ -0,0 +1,27 @@
+/**
+ * Beecrowd Problem 1002 - Area of a Circle
+ * https://www.beecrowd.com.br/judge/en/problems/view/1002
+ *
+ * Description:
+ * Read the value of the radius (R) of a circle and calculate its area.
+ *
+ * Formula:
+ * area = π * R²
+ *
+ * In this problem, π = 3.14159.
+ *
+ * Output:
+ * Print the area formatted as:
+ * A=area
+ * with 4 decimal places.
+ */
+
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+const R = parseFloat(lines[0]);
+const n = 3.14159;
+const A = n*(R*R);
+
+console.log(`A=${A.toFixed(4)}`);

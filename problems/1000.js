0df7a6a80049edcf8a1556103f6999589d14c3ff
Jhon Gomes <jhon.gomes1368@gmail.com>
2026-03-13 23:26:16 -0300

    Update 1000.js

--- problems/1000.js
+++ problems/1000.js
@@ -1,1 +1,9 @@
+/**
+ * Beecrownd Problem 1000 - Extremely Basic
+ * https://judge.beecrowd.com/en/problems/view/1000
+ *  
+ * Description:
+ *  In this first problem all you have to do is print this message "Hello World!" on the screen.
+ * */ 
 
+console.log("Hello World!")
